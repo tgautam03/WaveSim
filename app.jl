7ff08e44e298132c52cec0e425baa0ce66cf5c5d
tgautam03 <tushariitr3@gmail.com>
2024-05-03 17:19:13 -0600

    app.jl added

---
+++ app.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.19.41
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d5e4988a-09a0-11ef-2eb9-97277c169c65
+begin
+    import Pkg
+    # activate the shared project environment
+    Pkg.activate(Base.current_project())
+    # instantiate, i.e. make sure that all packages are downloaded
+    Pkg.instantiate()
+
+    import WaveSim
+end
+
+# ╔═╡ 206b9281-0aab-435d-8592-6e5169449832
+md"""
+# Activate Local Environment
+"""
+
+# ╔═╡ a2939c30-789f-4e22-842e-41477d733339
+
+
+# ╔═╡ Cell order:
+# ╟─206b9281-0aab-435d-8592-6e5169449832
+# ╠═d5e4988a-09a0-11ef-2eb9-97277c169c65
+# ╠═a2939c30-789f-4e22-842e-41477d733339
