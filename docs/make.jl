a07d1dc28569b21a385f56f7b47563eee2f69d6b
tgautam03 <tushariitr3@gmail.com>
2024-05-06 21:34:20 -0600

    Added sub folders

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,16 @@ using WaveSim
 makedocs(
     sitename = "WaveSim",
     format = Documenter.HTML(),
-    modules = [WaveSim]
+    modules = [WaveSim],
+    pages = [
+        "Home" => "index.md",
+        "1D Wave Equation" => Any[
+            "1d_wave/1d_FDM.md",
+        ],
+        "2D Wave Equation" => Any[
+            "2d_wave/2d_FDM.md",
+        ],
+    ],
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
